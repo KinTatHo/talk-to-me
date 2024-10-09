75c990ed2b78e5b23f1d7a13f6bd1ad677e70929
Kin Tat <110956850+KinTatHo@users.noreply.github.com>
2024-10-09 16:26:27 -0300

    Fix issue where app did not work

--- talk-to-me/frontend/src/App.js
+++ talk-to-me/frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import Header from "./components/Header";
